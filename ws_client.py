d386a160aa96167ddac03b946bf3bd870bfeeaf7
John Naulty <jnaulty@gmail.com>
2016-09-10 23:54:54 +0000

    rename + gitignore

--- ws_client.py
+++ ws_client.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from ws4py.client.tornadoclient import TornadoWebSocketClient
 from tornado import ioloop
 import json
@@ -24,7 +25,8 @@ class WSClient(TornadoWebSocketClient):
     def closed(self, code, reason=None):
         ioloop.IOLoop.instance().stop()
 
-ws = WSClient('ws://node6.getcloudbrain.com:31415/rt-stream/websocket', protocols=['http-only', 'chat', 'websocket'])
+WS_SERVER='ws://node6.getcloudbrain.com:31415/rt-stream/websocket'
+ws = WSClient(WS_SERVER, protocols=['http-only', 'chat', 'websocket'])
 ws.connect()
 
 ioloop.IOLoop.instance().start()
